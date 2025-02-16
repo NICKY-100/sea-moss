4207f6283925422332342bf797d4670badca1e4a
nicky <geesin11@gmail.com>
2025-02-16 17:46:21 +0000

    edit functions, adding comments for each product

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,40 @@
+import updateCart from "./updatecart.js";
+/**
+ * @import {CartItem} from './updatecart.js'
+ */
+
+const products = {
+
+    bay_leaves: {
+        name: "bay leaves",
+        price: 200
+    },
+    cinnamon_leaves: {
+        name: "cinnamon leaves",
+        price: 100
+    },
+    guava_leaves: {
+        name: "guava leaves",
+        price: 100
+    },
+    sea_moss_1: {
+        name: " sea moss",
+        price: 100
+    },
+    soursop_leaves: {
+        name: "soursop leaves",
+        price: 200
+    }
+
+};
+
+/** @type {CartItem[]} */
+const cartItems = JSON.parse(localStorage.getItem("cart")) || []
+/**
+ * 
+ */
 function displayCart() {
-    const cartPage = document.getElementById("cartpage");
+    const cartPage = document.getElementById("cartPage");
     console.log(cartItems);
 
     let qtyTotal = 0;
@@ -7,36 +42,42 @@ function displayCart() {
     for (let i = 0; i < cartItems.length; i++) {
         const cartItem = cartItems[i];
         const product = document.querySelector(
-            "#product-" + cartItem["product-id"]
+            "#product-" + cartItem.productId
         );
 
-        const productId = cartItem["product-id"];
+        const productId = cartItem.productId;
 
-        cartItemTotal += cartItem.qty * products[productId].price;
-        qtyTotal += cartItem.qty;
+        cartItemTotal += cartItem.quantity * products[productId].price;
+        qtyTotal += cartItem.quantity;
 
         if (!product) {
-
+            /**
+             * to create a cartItem that displays the name, price, quantity
+             */
             const qtyText = new Text("qty: ");
             const priceText = new Text("price: ");
-            const div = document.createElement("div");
+            const li = document.createElement("li");
             const h4 = document.createElement("h4");
             const qty = document.createElement("span");
             const price = document.createElement("span");
 
-            div.append(h4, qtyText, qty, priceText, price);
-            cartPage.appendChild(div);
+            li.append(h4, qtyText, qty, priceText, price);
+            cartPage.appendChild(li);
 
-            div.id = "product-" + cartItem["product-id"];
-            qty.textContent = cartItem.qty;
+            li.id = "product-" + cartItem.productId;
+            li.classList.add("list-group-item")
+            qty.textContent = cartItem.quantity.toString();
             qty.classList.add("qty");
             price.classList.add("price");
-            h4.textContent = products[cartItem["product-id"]].name;
-            price.textContent = products[cartItem["product-id"]].price;
+            h4.textContent = products[cartItem.productId].name;
+            price.textContent = products[cartItem.productId].price;
 
         } else {
+            /**
+             * updating the cartItem
+             */
             const productQty = product.querySelector(".qty");
-            productQty.textContent = cartItem.qty;
+            productQty.textContent = cartItem.quantity.toString();
             const productPrice = product.querySelector(".price");
             console.log(productQty);
         }
@@ -44,8 +85,10 @@ function displayCart() {
 
     const items = document.getElementById("cart-item-total");
 
-    items.textContent = qtyTotal;
+    items.textContent = qtyTotal.toString();
     console.log(cartItemTotal);
     const total = document.getElementById("item-total");
-    total.textContent = cartItemTotal;
+    total.textContent = cartItemTotal.toString();
 }
+updateCart(cartItems)
+displayCart()
